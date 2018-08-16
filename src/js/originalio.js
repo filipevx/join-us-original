a691a11624182efc6c4ddcc7f999f8a852cb552e
Filipe Xavier <filipevx@gmail.com>
2018-08-15 21:35:21 -0300

    Ajustes Produto

--- src/js/originalio.js
+++ src/js/originalio.js
@@ -15,5 +15,13 @@ $( document ).ready(function() {
     $('.js-modal-open, .icon-close, .overlay-modal, .finish-btn, .keepbuy-btn').click(function(){ 
     	$('.modal').toggleClass('modal--open')
     	$('.overlay-modal').toggleClass('overlay-modal--open')
-    });  
-});
+    });
+	$('.size-btns__button').click(function(){
+	  $('.size-btns__button').removeClass('size-btns__button--select');
+	  $(this).addClass('size-btns__button--select');     
+	}); 
+	$('.color-btns__button').click(function(){
+	  $('.color-btns__button').removeClass('color-btns__button--select');
+	  $(this).addClass('color-btns__button--select');     
+	}); 	        
+});  
